752fbca38c3c7504c4a303859776b479a4d067d3
pscore23 <103930835+pscore23@users.noreply.github.com>
2022-08-22 15:47:56 +0900

    Update Sample1.py

--- Sample1.py
+++ Sample1.py
@@ -8,8 +8,10 @@ headers = {
 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
 }
 
+
 def get_unread_msg_cnt(user):
     headers["referer"] = "https://scratch.mit.edu"
     return requests.get("https://api.scratch.mit.edu/users/" + str(user) + "/messages/count", headers=headers).json()['count']
+
 
 print(get_unread_msg_cnt(input("Username: ")))
